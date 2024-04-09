a3437b83815d3d879e6969dbb5e56e6d30d23f8e
feeling-alive <144514639+feeling-alive@users.noreply.github.com>
2024-04-09 21:09:59 +0300

    Add files via upload

--- UP/find me.py
+++ UP/find me.py
@@ -2,6 +2,8 @@ from PyQt6.QtWidgets import QMainWindow, QLabel, QWidget, QApplication, QGridLay
 from PyQt6.QtGui import QPixmap, QBrush, QPalette
 from PyQt6.QtCore import Qt
 import sys
+import random
+
 
 class GameWindow(QMainWindow):
 
@@ -16,13 +18,16 @@ class GameWindow(QMainWindow):
 
         self.grid = QGridLayout(central_widget)
 
-        pixmap2 = QPixmap("chel1.png").scaled(350, 200)
-        self.character_label1 = QLabel()
-        self.character_label1.setPixmap(pixmap2)
-        self.character_label1.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.grid.addWidget(self.character_label1, 0, 0)
+        self.image_paths = ['ch0.png', 'ch1.png', 'ch2.png']
+
+        self.background_label = QLabel(self)
+
 
         self.load_background_image()
+
+        self.characters = []
+        self.print_heroes()
+
 
     def load_background_image(self):
         pixmap = QPixmap("start.png").scaled(1080, 720)
@@ -30,6 +35,32 @@ class GameWindow(QMainWindow):
         palette = self.palette()
         palette.setBrush(QPalette.ColorRole.Window, brush)
         self.setPalette(palette)
+
+        self.background_label.setPixmap(pixmap)
+        self.background_label.setGeometry(0, 0, pixmap.width(), pixmap.height())
+        self.background_label.show()
+
+    def is_inside_rhombus(self, x, y, xc, yc, d):
+        return abs(x - xc) / d + abs(y - yc) / d <= 1
+
+    def print_heroes(self):
+        xc = 530
+        yc = 250
+        d = 350
+
+        for i, image_path in enumerate(self.image_paths):
+            pixmap = QPixmap(image_path)
+            character_label = QLabel(self)
+            character_label.setPixmap(pixmap)
+            character_label.setGeometry(0, 0, pixmap.width(), pixmap.height())
+
+            x, y = random.randint(xc - d, xc + d), random.randint(yc - d, yc + d)
+            while not self.is_inside_rhombus(x, y, xc, yc, d):
+                x, y = random.randint(xc - d, xc + d), random.randint(yc - d, yc + d)
+
+            character_label.move(x, y)
+            character_label.show()
+            self.characters.append(character_label)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)

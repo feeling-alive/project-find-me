62fc3f9d044fe5e4761ac211fbbb27902aaaa4ad
feeling-alive <144514639+feeling-alive@users.noreply.github.com>
2024-06-17 11:03:10 +0300

    Полностью исправная версия

--- UP/find me.py
+++ UP/find me.py
@@ -5,12 +5,13 @@ from window import InputDialog
 import sys
 import csv
 import pandas as pd
-import time
 import random
 
+global end_window_reference
+end_window_reference = None
 
+#Создание записей
 class TableModel(QAbstractTableModel):
-
     def __init__(self, data):
         super(TableModel, self).__init__()
         self._data = data
@@ -30,53 +31,26 @@ class TableModel(QAbstractTableModel):
         if role == Qt.ItemDataRole.DisplayRole:
             if orientation == Qt.Orientation.Horizontal:
                 return str(self._data.columns[section])
-
             if orientation == Qt.Orientation.Vertical:
                 return str(self._data.index[section])
+        if role == Qt.ItemDataRole.TextAlignmentRole:
+            return Qt.AlignmentFlag.AlignCenter
 
-
+    def removeRows(self, row, count, parent):
+        self.beginRemoveRows(parent, row, row + count - 1)
+        self._data = self._data.drop(self._data.index[row:row + count])
+        self.endRemoveRows()
+        self.layoutChanged.emit()
+        return True
+#Создание таблицы отображение всех записей в ней так же их удаление
 class Scoreboard(QMainWindow):
     def __init__(self):
         super().__init__()
-        input_dialog = InputDialog()
-        input_dialog.show()
-        if input_dialog.exec() == QDialog.DialogCode.Accepted:
-            self.text = input_dialog.getText()
-        else:
-            self.text = "Неизвестный пользователь"
-
-        menu_bar = self.menuBar()
-        score = menu_bar.addAction("Таблица")
-        score.triggered.connect(self.table)
-
-        self.setStatusBar(QStatusBar(self))
-
-        menu_bar.setStyleSheet("""
-               *{
-               background-color:white;
-               font-size:13px;
-               font-color:#343155;
-               font-weight:bold;
-               }
-               """)
-
-        pageLayout = QVBoxLayout()
-        self.topLayout = QHBoxLayout(self)
-        self.outerLayout = QHBoxLayout(self)
-        self.frame = QFrame()
-        self.frame.setStyleSheet("""
-                   border-width: 6px;
-                   border-height: 10px;
-                   border-radius:4px;
-                   font-size:20px;
-                   font-weight:bold;
-                   color: #F8F8FF;
-               """)
         self.setWindowTitle("Таблица лидеров")
         self.index = []
         self.table = QTableView()
+        self.setStyleSheet("background-color:#082567;")
         self.data = []
-        self.setFixedWidth(254)
 
         with open("newlb.csv", "r", encoding="utf-8") as f:
             reader = csv.DictReader(f)
@@ -87,27 +61,98 @@ class Scoreboard(QMainWindow):
 
         for i in range(len(self.data) - 1):
             for j in range(len(self.data) - 1 - i):
-                if int(self.data[j][1]) < int(self.data[j + 1][1]):
+                if int(self.data[j][1].strip()) < int(self.data[j + 1][1].strip()):
                     self.data[j], self.data[j + 1] = self.data[j + 1], self.data[j]
 
         for i in range(1, len(self.data) + 1):
             self.index.append(f"{i} место")
-        data = pd.DataFrame(self.data,
-                            columns=['Имя', 'Очки'], index=self.index
-                            )
+
+        data = pd.DataFrame(self.data, columns=['Имя', 'Очки'], index=self.index)
         self.model = TableModel(data)
         self.table.setModel(self.model)
-        self.setCentralWidget(self.table)
+        self.table.setStyleSheet("""
+        QTableView {
+            background-color: #0d6efd;
+            font-size: 15px;
+            color: white;
+            font-family: "Better VCR";
+            selection-background-color: #0b5ed7;
+        }
+        QHeaderView::section {
+            background-color: #0d6efd;
+            font-size: 15px;
+            color: white;
+            font-family: "Better VCR";
+            padding: 6px;
+            border-bottom: 1px solid #343155;
+        }
+        QTableView::viewport {
+            background-color: #0d6efd;  
+        }
+        QTableView::cornerButton {
+            background-color: #0d6efd;  
+        }
+        """)
 
-    def table(self):
-        if self.a is None:
-            self.a = Scoreboard()
-            self.a.show()
-        else:
-            self.a.close()
-            self.a = None
+        self.btn_delete = QPushButton("Удалить запись")
+        self.btn_delete.setStyleSheet("""
+        *{
+            background-color:#0d6efd;
+            border-radius:10px;
+            padding:6px ;
+            border:1px solid #343155;  
+            color:#fff;
+            margin-top:1px;
+            font-size:16px;
+            font-weight:bold;
+            font-family: Better VCR;
+        }
+        *:hover{
+            background-color:#0b5ed7;
+            border:1px solid #9ac3fe;
+        }
+        """)
+        self.btn_delete.clicked.connect(self.delete_row)
 
+        layout = QVBoxLayout()
+        layout.addWidget(self.table)
+        layout.addWidget(self.btn_delete)
 
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def delete_row(self):
+        selected_index = self.table.selectionModel().currentIndex()
+        if selected_index.isValid():
+            self.model.removeRows(selected_index.row(), 1, selected_index)
+            row_to_delete = selected_index.row()
+
+            with open("newlb.csv", "r", encoding="utf-8") as f:
+                records = list(csv.DictReader(f))
+
+            with open("newlb.csv", "w", newline='', encoding="utf-8") as f:
+                fieldnames = ['Name', 'Count']
+                writer = csv.DictWriter(f, fieldnames=fieldnames)
+                writer.writeheader()
+
+                for idx, row in enumerate(records):
+                    if idx != row_to_delete:
+                        writer.writerow(row)
+
+            with open("lb.csv", "r", encoding="utf-8") as f:
+                records = list(csv.DictReader(f))
+
+            with open("lb.csv", "w", newline='', encoding="utf-8") as f:
+                fieldnames = ['Name', 'Count']
+                writer = csv.DictWriter(f, fieldnames=fieldnames)
+                writer.writeheader()
+
+                for idx, row in enumerate(records):
+                    if idx != row_to_delete:
+                        writer.writerow(row)
+
+#Окно с правилами
 class helpWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -121,8 +166,8 @@ class helpWindow(QWidget):
                             "\nС каждым уровнем количество различных персонажей \nна поле увеличивается, они могут быть"
                             " одинаковыми, "
                             " искомый персонаж остаётся неизменным."
-                            "\nПо истечению определённого колличества раундов время \nна таймере уменьшится на 15 секунд,"
-                            " минимальное время таймера 15 секунд."
+                            "\nПо истечению 5-7 раундов время на таймере \nуменьшится на 15 секунд,"
+                            " максимальное время таймера 15 секунд."
                             "\nЗа каждое правильное нахождение игрок получает баллы, \nчем меньше максимальное время"
                             " таймера, тем больше очков игрок получит. "
                             "\nИгра завершится по истечению времени \nили по окончанию попыток на нахождение "
@@ -135,7 +180,7 @@ class helpWindow(QWidget):
 
         self.setLayout(layout)
 
-
+#Начальное окно с кнопкой старт и кнопкой правила
 class Screen1(QMainWindow):
     def __init__(self):
         super(Screen1, self).__init__()
@@ -143,6 +188,12 @@ class Screen1(QMainWindow):
         self.setFixedSize(1080, 720)
         self.background_label = QLabel(self)
         self.load_background_image()
+        input_dialog = InputDialog()
+        input_dialog.show()
+        if input_dialog.exec() == QDialog.DialogCode.Accepted:
+            self.text = input_dialog.getText()
+        else:
+            self.text = "Неизвестный пользователь"
         self.button = QPushButton('Старт', self)
         self.button.clicked.connect(self.next_scr)
         self.button.move(420, 470)
@@ -186,15 +237,16 @@ class Screen1(QMainWindow):
 
     def next_scr(self):
         self.hide()
-        game_window.show()
+        self.game_window = GameWindow(self.text)  # Создаём экземпляр GameWindow здесь и передаём имя пользователя.
+        self.game_window.show()
 
-
+#Окно игрового процесса
 class GameWindow(QMainWindow):
     cp = pyqtSignal(int, int)
 
-    def __init__(self):
+    def __init__(self, name):
         super().__init__()
-
+        self.player_name = name
         self.image_paths = ['sprite\ch0.png']
         self.ostch = ['sprite\ch1.png',
                       'sprite\ch2.png',
@@ -255,6 +307,7 @@ class GameWindow(QMainWindow):
         self.initUI()
 
     def end_game(self):
+        global end_window_reference
         if self.dc == 1:
             self.dead.setPixmap(QPixmap("sprite\dead1.png"))
         if self.dc == 2:
@@ -262,8 +315,9 @@ class GameWindow(QMainWindow):
         if self.dc == 3:
             self.dead.setPixmap(QPixmap("sprite\dead3.png"))
             self.close()
-            end_window.show()
             self.timer.stop()
+            end_window_reference = EndWindow(self.player_name, self.score_count)
+            end_window_reference.show()
 
     def initUI(self):
         self.setWindowTitle("Find Me")
@@ -275,7 +329,7 @@ class GameWindow(QMainWindow):
         self.timer.timeout.connect(self.update_timer)
         self.timer.start(1000)
         self.timer.stop()
-
+#Функция добавления очков
     def score_up(self):
         if self.time_uper == 60:
             self.score_count += 100
@@ -287,7 +341,9 @@ class GameWindow(QMainWindow):
             self.score_count += 400
         self.score.setText(f"{self.score_count:06}")
 
+#Функция таймера
     def update_timer(self):
+        global end_window_reference
         if self.timer_started == False:
             self.timer.stop()
         else:
@@ -296,16 +352,24 @@ class GameWindow(QMainWindow):
         if self.countdown_time == 0:
             self.timer.stop()
             self.close()
-            end_window.show()
+            end_window_reference = EndWindow(self.player_name, self.score_count)
+            end_window_reference.show()
 
+#Функция поска персонажа по клику
     def on_character_clicked(self, x, y):
         if not self.timer_started:
             self.timer_started = True
-        for character in self.characters:
+
+        index = 0
+        search = False
+
+        while index < len(self.characters):
+            character = self.characters[index]
+
             if character.underMouse():
-                if character.pixmap().toImage() == QPixmap(
-                        "sprite\ch0.png").toImage():
+                if character.pixmap().toImage() == QPixmap("sprite\ch0.png").toImage():
                     print(True)
+                    self.search = True
                     self.characters.remove(character)
                     character.hide()
                     new_characters_count = random.randint(3, 5)
@@ -321,10 +385,16 @@ class GameWindow(QMainWindow):
                             self.timer_label.setText(f"{self.countdown_time}")
                             self.rounds_played = 0
                             self.rounds_to_next_reduction = random.randint(5, 7)
+                    search = True
+                    break
                 else:
-                    print(False)
-                    self.dc += 1
-                    self.end_game()
+                    if not search:
+                        print(False)
+                        self.dc += 1
+                        self.end_game()
+                        search = True
+            index += 1
+
 
     def timer_rearm(self, countdown_time):
         self.countdown_time = countdown_time
@@ -333,10 +403,11 @@ class GameWindow(QMainWindow):
         if self.countdown_time == 0:
             self.timer.stop()
 
+#Добавление персонажа
     def new_pers(self):
-        xc = 520
-        yc = 260
-        d = 290
+        xc = 525
+        yc = 300
+        d = 300
 
         for _ in range(1):
             pixmap = QPixmap(random.choice(self.image_paths))
@@ -352,10 +423,11 @@ class GameWindow(QMainWindow):
             character_label.show()
             self.characters.append(character_label)
 
+# Добавление персонажей
     def add_new_characters(self, count):
-        xc = 515
-        yc = 255
-        d = 285
+        xc = 525
+        yc = 300
+        d = 300
 
         for _ in range(count):
             pixmap = QPixmap(random.choice(self.ostch))
@@ -372,12 +444,14 @@ class GameWindow(QMainWindow):
             self.characters.append(character_label)
         self.new_pers()
 
+#Отслеживание клика мыши и вывод координат
     def mousePressEvent(self, e):
         p = e.pos()
         global_pos = self.mapToGlobal(p)
         print(p.x(), p.y())
 
         self.cp.emit(p.x(), p.y())
+
 
     def load_background_image(self):
         pixmap = QPixmap("sprite\start.png").scaled(1080, 720)
@@ -389,13 +463,14 @@ class GameWindow(QMainWindow):
         self.background_label.setPixmap(pixmap)
         self.background_label.setGeometry(0, 0, pixmap.width(), pixmap.height())
         self.background_label.show()
-
+#Нахождение границ ромба
     def is_inside_rhombus(self, x, y, xc, yc, d):
         return abs(x - xc) / d + abs(y - yc) / d <= 1
 
+# Добавление персонажа
     def print_heroes(self):
-        xc = 520
-        yc = 260
+        xc = 525
+        yc = 300
         d = 300
 
         for i, image_path in enumerate(self.image_paths):
@@ -413,15 +488,18 @@ class GameWindow(QMainWindow):
             character_label.show()
             self.characters.append(character_label)
 
-
+#Окно окончания с кнопками перезапуск, выход и таблица рекордов
 class EndWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, player_name, player_score):
         super(EndWindow, self).__init__()
-
         self.setWindowTitle("Find Me")
+        self.a = None
+        self.add_to_table(player_name, player_score)
         self.setFixedSize(1080, 720)
         self.background_label = QLabel(self)
         self.load_background_image()
+        self.player_name = player_name
+        self.player_score = player_score 
         self.rebutton = QPushButton('Перезапуск', self)
         self.rebutton.clicked.connect(self.restart)
         self.rebutton.move(120, 470)
@@ -430,13 +508,12 @@ class EndWindow(QMainWindow):
                                     "font-size: 20px; font-family: Better VCR")
 
         self.scoreboard_button = QPushButton('Таблица Рекордов', self)
-        self.scoreboard_button.clicked.connect(self.show_scoreboard)
         self.scoreboard_button.move(420, 470)
         self.scoreboard_button.resize(250, 60)
         self.scoreboard_button.setStyleSheet(
             "background-color: #4CAF50; color: white; border-radius: 10px; font-weight: bold; "
             "font-size: 18px; font-family: Better VCR")
-        self.scoreboard_button.clicked.connect(self.show_scoreboard)
+        self.scoreboard_button.clicked.connect(self.table)
 
         self.endbutton = QPushButton('Выход', self)
         self.endbutton.clicked.connect(self.end)
@@ -450,9 +527,27 @@ class EndWindow(QMainWindow):
         self.label.setMovie(movie)
         movie.start()
 
-    def show_scoreboard(self):
-        self.scoreboard_window = Scoreboard()
-        self.scoreboard_window.show()
+    def add_to_table(self, name, count):
+        with open("lb.csv", "a", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow([name, count])
+        file.close()
+
+        with (open("lb.csv", "r", encoding="utf-8") as r,
+              open("newlb.csv", "w", encoding="utf-8") as o):
+            for line in r:
+                if line.strip():
+                    o.write(line)
+        r.close()
+        o.close()
+
+    def table(self):
+        if self.a is None:
+            self.a = Scoreboard()
+            self.a.show()
+        else:
+            self.a.close()
+            self.a = None
 
     def load_background_image(self):
         pixmap = QPixmap("sprite\start2.png").scaled(1080, 720)
@@ -469,22 +564,15 @@ class EndWindow(QMainWindow):
         exit()
 
     def restart(self):
-        global game_window
-        game_window.close()
-        game_window = GameWindow()
-        game_window.show()
+        global end_window_reference
         self.close()
+        end_window_reference = GameWindow(
+            self.player_name)
+        end_window_reference.show()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
-    scoreboard = Scoreboard()
-    scoreboard.show()
-
     screen1 = Screen1()
-    game_window = GameWindow()
-    end_window = EndWindow()
     screen1.show()
-
     sys.exit(app.exec())
